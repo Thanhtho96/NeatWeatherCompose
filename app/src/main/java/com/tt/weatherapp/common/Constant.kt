555b235d22a1e127dffdb1f60254aa6bf8cc816e
ThanhTho <thanhbat96@gmail.com>
2022-06-23 13:31:26 +0700

    - set debuggable to false for release

--- app/src/main/java/com/tt/weatherapp/common/Constant.kt
+++ app/src/main/java/com/tt/weatherapp/common/Constant.kt
@@ -1,12 +1,12 @@
 package com.tt.weatherapp.common
 
+import androidx.annotation.Keep
+
 object Constant {
     const val BASE_URL = "https://api.openweathermap.org/data/2.5/"
-    const val ENGLISH = "en"
-    const val VIETNAMESE = "vi"
-
     fun getWeatherIcon(icon: String) = "https://openweathermap.org/img/wn/${icon}@2x.png"
 
+    @Keep
     enum class Unit(val value: String) {
         METRIC("Metric"),
         IMPERIAL("Imperial");
